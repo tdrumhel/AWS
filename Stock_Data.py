e5c1dc6928f8ae2655cd4830d64d602405a87a39
tdrumhel <tyler.drumheller@gmail.com>
2016-08-28 21:02:01 -0400

    Additional Logic Function & Ticker Analysis

--- Stock_Data.py
+++ Stock_Data.py
@@ -45,7 +45,55 @@ def Transactions(f):
         elif transactions[key][1] == 'Sell':
             Sell_Total += transactions[key][0]*100
             Sell_Count += 1
-    return Sell_Total - Buy_Total, Buy_Count, Sell_Count, transactions
+
+    #Selling Last Purchase if at End of Date Range
+    if transactions[max_tran_date][1] == 'Buy':
+        Sell_Total += transactions[max_tran_date][0]*100
+        Sell_Count += 1
+
+    return Sell_Total - Buy_Total, (Buy_Total/Buy_Count), Buy_Count, Sell_Count, transactions
+
+def Rolling_Average_Intersection(f):
+
+    buy_indicator = 0
+    transactions = {}
+    Last_Transaction_Index = 0
+    Last_Rolling_40 = 10000
+    Last_Rolling_120 = 0
+
+    #Determining when to Buy and Sell
+    for index, row in f.iterrows():
+        if row['40_Day'] > row['120_Day'] and Last_Rolling_40 < row['120_Day'] and buy_indicator == 0 and row['date_index'] > 120:
+            transactions[row['Date']] = [row['Close'], 'Buy']
+            Last_Transaction_Index = row['date_index']
+            buy_indicator = 1
+        if row['40_Day'] < row['120_Day'] and Last_Rolling_120 > row['40_Day'] and buy_indicator == 1:
+            transactions[row['Date']] = [row['Close'], 'Sell']
+            buy_indicator = 0
+        Last_Rolling_40 = row['40_Day']
+        Last_Rolling_120 = row['120_Day']
+
+    Buy_Total = 0
+    Sell_Total = 0
+    Buy_Count = 0
+    Sell_Count = 0
+
+    #Determining Profit and Total Transactions
+    for key in transactions:
+        if transactions[key][1] == 'Buy':
+            Buy_Total += transactions[key][0]*100
+            Buy_Count += 1
+        elif transactions[key][1] == 'Sell':
+            Sell_Total += transactions[key][0]*100
+            Sell_Count += 1
+    max_tran_date = max(transactions.keys())
+
+    #Selling Last Purchase if at End of Date Range
+    if transactions[max_tran_date][1] == 'Buy':
+        Sell_Total += transactions[max_tran_date][0]*100
+        Sell_Count += 1
+
+    return Sell_Total - Buy_Total, (Buy_Total/Buy_Count), Buy_Count, Sell_Count, transactions
 
 def Stock_Plot(f, transactions):
 
